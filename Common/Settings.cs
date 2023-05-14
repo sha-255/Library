09359da0ce904da71b3f3c51685659a703aec73f
sha-255 <fawsgre@mail.ru>
2023-05-14 20:35:26 +0600

    Initial commit

---
+++ Common/Settings.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System;
+
+namespace AdvertisingAgency.Common
+{
+    public static class Settings
+    {
+        public static string SqlConnection { get; private set; }
+        private static string Path = Environment.CurrentDirectory;
+        private static string SubPath = @"appSettings.txt";
+        private static string FullPath = Path + @$"\{SubPath}";
+        private static bool Exists = File.Exists(FullPath);
+
+        public static void Apply()
+        {
+            if (!Exists)
+                File.Create("appSettings.txt");
+            SqlConnection = new StreamReader(FullPath).ReadToEnd();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using AdvertisingAgency.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Library
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Initialized += (s, e) => Settings.Apply();
+        }
+    }
+}
