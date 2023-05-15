9d915725ed12616415e2af5d132f1b98b28dd396
sha-255 <100027136+sha-255@users.noreply.github.com>
2023-05-15 15:54:33 +0600

    Add style

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using AdvertisingAgency.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Library.Pages;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Library
 {
@@ -24,6 +12,7 @@ namespace Library
         public MainWindow()
         {
             InitializeComponent();
+            MainFrame.Navigate(new Readers());
             Initialized += (s, e) => Settings.Apply();
         }
     }
---
+++ Pages/Readers.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Library.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Readers.xaml
+    /// </summary>
+    public partial class Readers : Page
+    {
+        public Readers()
+        {
+            InitializeComponent();
+        }
+    }
+}
